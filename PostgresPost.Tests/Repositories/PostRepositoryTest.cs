d5697285e8a09900a3b77475bcd6f611b75e0cc9
Sergey Chevychelov <sergeyche46@mail.ru>
2024-02-24 21:46:55 +0300

    Тесты

---
+++ PostgresPost.Tests/Repositories/PostRepositoryTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using PostgresPosts.Models.Posts;
+using PostgresPosts.Models.Repositories;
+using Xunit;
+
+namespace PostgresPost.Tests.Repositories
+{
+    public class PostRepositoryTest
+    {
+        private IRepository<PostPostViewModel, PutPostViewModel> _repository;
+        public PostRepositoryTest()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json")
+                .Build();
+            _repository = new PostRepository(configuration);
+        }
+
+        [Fact]
+        public void IRepository_ImplementsRightConcreteClass()
+        {
+            Assert.IsAssignableFrom<IRepository<PostPostViewModel, PutPostViewModel>>(_repository);
+            Assert.IsAssignableFrom<BaseRepository>(_repository);
+            Assert.NotNull(_repository.GetAll());
+        }
+
+    }
+}
